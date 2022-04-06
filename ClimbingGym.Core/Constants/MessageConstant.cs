f85a16dd3e848d7812bc1dd9aac9599f24891744
Jul <Jul@DESKTOP-0USLRRR>
2022-04-06 15:26:51 +0300

    Add project files.

---
+++ ClimbingGym.Core/Constants/MessageConstant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClimbingGym.Core.Constants
+{
+    public class MessageConstant
+    {
+        public const string ErrorMessage = "Error Message";
+        public const string SuccessMessage = "Success Message";
+        public const string WarningMessage = "Warning Message";
+    }
+}
---
+++ ClimbingGym/ModelBinders/DateTimeModelBinderProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace ClimbingGym.ModelBinders
+{
+    public class DateTimeModelBinderProvider : IModelBinderProvider
+    {
+        private readonly string customeDateFormat;
+        public DateTimeModelBinderProvider(string _customeDateFormat)
+        {
+            customeDateFormat = _customeDateFormat;
+        }
+        public IModelBinder? GetBinder(ModelBinderProviderContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (context.Metadata.ModelType == typeof(DateTime) || context.Metadata.ModelType == typeof(DateTime?))
+            {
+                return new DateTimeModelBinder(customeDateFormat);
+            }
+            return null;
+        }
+    }
+}
